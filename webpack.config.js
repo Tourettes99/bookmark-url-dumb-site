e96f58c1a2e9945e3dd9a91b11ea253038b3bea5
Tourettes99 <107359988+Tourettes99@users.noreply.github.com>
2025-01-29 11:43:29 +0100

    Add netlify static version of bookmark site

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+    plugins: [
+        new Dotenv()
+    ],
+    // ... other webpack configurations
+}; 
